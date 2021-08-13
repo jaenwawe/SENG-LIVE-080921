088a3c67e88c6a6fba2d73f437372cdbc7153f3f
Aysan Isayo <aisayo@icloud.com>
2021-08-13 13:14:57 -0700

    solution

--- phase-1/05-dom-manipulation/index.js
+++ phase-1/05-dom-manipulation/index.js
@@ -31,18 +31,64 @@ const pokemons = [
   },
 ];
 
-pokemons.forEach((pokemon) => renderPokemon(pokemon));
+// select poke form
+const pokeForm = document.getElementById('poke-form')
+// console.log(pokeForm)
 
-function renderPokemon(poke) {
-  const pokeCard = document.createElement("div");
-  pokeCard.id = `poke-${poke.id}`;
-  pokeCard.className = "poke-card";
+const labels = document.getElementsByClassName('form-label')
+// console.log(labels)
 
-  const pokeImg = document.createElement("img");
-  pokeImg.src = poke.img;
-  pokeImg.alt = `${poke.name} image`;
+const goalsDiv = document.querySelector('#lecture-goals')
+// console.log(goalsDiv)
 
-  pokeCard.appendChild(pokeImg);
-  const pokeContainer = document.querySelector("#poke-container");
-  pokeContainer.appendChild(pokeCard);
+const lis = document.querySelectorAll('li')
+// console.log(lis)
+// pokemons.forEach((pokemon) => renderPokemon(pokemon))
+
+
+pokemons.forEach(renderPokemon)
+
+function renderPokemon(pokemon){
+   console.log(pokemon)
+  // debugger;
+  const pokeCard = document.createElement("div")
+  pokeCard.id = 'poke-' + pokemon.id
+  pokeCard.className = "poke-card"
+
+  // create img element
+  // give img its properties
+  // add this img to pokeCard
+
+  const pokeImg = document.createElement("img")
+  pokeImg.src = pokemon.img
+  pokeImg.alt = `${pokemon.name} image`
+  
+  pokeCard.appendChild(pokeImg)
+  
+  // take pokeCard and attach to 'poke-container'
+  const pokeContainer = document.getElementById('poke-container')
+  // document.querySelector('#poke-container')
+
+  pokeContainer.appendChild(pokeCard)
+  // pokeContainer.append(pokeCard)
+  // pokeContainer.innerHTML += pokeCard
+  
 }
+
+const h1 = document.querySelector('div#lecture-goals h1')
+h1.textContent = "Making things appear on the web"
+
+// innerHTML
+// innerText
+// textContent
+
+goalsDiv.innerHTML += `<button>Click Me!!</button>`
+
+const header = document.querySelector('#header h1')
+header.innerHTML = `<img id="header-img" src="https://external-preview.redd.it/tQged7mKJ3cUpNMq5IMeceZvyKP3cTyHqhNmKEQ0Vv8.png?auto=webp&s=fb5fd61cae0bc9cde2bc2a006b1e2aeb0c935ce9"/>`
+
+document.querySelector('button').remove()
+
+goalsDiv.remove()
+
+// document.getElementById('lecture-goal').remove()
