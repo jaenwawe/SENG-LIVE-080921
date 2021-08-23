78c0a7eccb50627921cec9c0e07b3fc7052b711a
Aysan Isayo <aisayo@icloud.com>
2021-08-23 14:44:26 -0700

    lecture solution

--- phase-1/10-fetch-patch-delete/README.md
+++ phase-1/10-fetch-patch-delete/README.md
@@ -11,6 +11,21 @@ json-server --watch db.json
 
 Go over solution for Friday's exercise. This is a good time to review everything with the students
 
+### Exercise Deliverables:
+
+- For this exercise, we will create an interface that allows users to comment on the collection of cards. 
+- We have an endpoint `http://localhost:3000/comments` that will host the collection of comments submitted by a user. We will use this endpoint for any requests made in respect to comments.
+- Currently we do not have the following: 
+  1. A form to submit a comment
+  2. A request made to the server after the form has been submitted. 
+  3. Do not worry about displaying the comments currently. For now you can console.log the response to the request and inspect the data that is received.
+
+- BONUS: If you have completed the above exercise, try to display the comments on the page. Do not worry about styling. You can be creative and display the comments as you would like. Things to think about: 
+  1. How to retreive all comments?
+  2. Where should the comments exist?
+  3. How to display comments? 
+
+
 ### Segment 1:
 
 Review code, review events and functions that pertain to the like button.
--- phase-1/10-fetch-patch-delete/index.js
+++ phase-1/10-fetch-patch-delete/index.js
@@ -1,5 +1,6 @@
 const pokeContainer = document.getElementById("poke-container");
 const pokeForm = document.getElementById("poke-form");
+const formDiv = document.getElementById("comments-form");
 
 function renderPokemon(pokemon) {
   const pokeCard = document.createElement("div");
@@ -28,7 +29,7 @@ function renderPokemon(pokemon) {
   const deleteBttn = document.createElement("button");
   deleteBttn.className = "delete-bttn";
   deleteBttn.textContent = "Delete";
-  deleteBttn.addEventListener("click", () => deletePoke(pokeCard));
+  deleteBttn.addEventListener("click", () => deletePoke(pokeCard, pokemon));
 
   pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likeBttn, deleteBttn);
   pokeContainer.appendChild(pokeCard);
@@ -40,28 +41,88 @@ function createPokemon(event) {
   let pokeName = pokeForm.querySelector("#name-input").value;
   let pokeImg = pokeForm.querySelector("#img-input").value;
 
-  let pokemon = {
-    name: pokeName,
-    img: pokeImg,
-    likes: 0,
-  };
+  // conditonal to check that form has values entered
+  // before post request
 
-  // optimistic rendering: rendering pokemon to dom before castPokemon(fetch request)
-  renderPokemon(pokemon);
-  castPokemon(pokemon);
-  pokeForm.reset();
+  // if the values are not blank
+  // make a post request, render the new pokemon
+  // else alert user to fill out form
+
+  if (pokeName && pokeImg){
+    let pokemon = {
+      name: pokeName,
+      img: pokeImg,
+      likes: 0,
+    };
+    // optimistic rendering: rendering pokemon to dom before castPokemon(fetch request)
+    renderPokemon(pokemon);
+    castPokemon(pokemon);
+    pokeForm.reset();  
+  } else {
+    alert('Please fill out the form!')
+  }
+
 }
 
 function increaseLike(pokemon, likesElement) {
+  // optimistic rendering 
   ++pokemon.likes;
   likesElement.textContent = pokemon.likes;
+  // console.log(likesElement.textContent)
+  // provide data that will be changed
+  // method, headers and body
+  
+  fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({likes: pokemon.likes})
+  })
+
+  
+  // console.log(likesElement.textContent)
+  // provide data that will be changed
+  // method, headers and body
+  
+  // pessimistic rendering
+  // fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+  //   method: 'PATCH',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({likes: pokemon.likes})
+  // })
+  // .then(resp => resp.json())
+  // .then(data => {
+  //   console.log(data)
+  //   likesElement.textContent = data.likes;
+  // })
 }
 
-function deletePoke(pokeCard) {
-  pokeCard.remove();
+function deletePoke(pokeCard, pokemon) {
+  // pokeCard.remove();
+
+  // // identify if we want to be pessimistic or optimistic
+  // // make our delete request
+
+  // //optimistic 
+  // fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+  //   method: 'DELETE'
+  // })
+  // .then()
+  // .then()
+
+  //pessimistic
+  fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+    method: 'DELETE'
+  })
+  .then(resp => resp.json())
+  .then(pokeCard.remove())
 }
 
 function init() {
+  createCommentForm();
   gottaCatchEmAll();
   pokeForm.addEventListener("submit", createPokemon);
 }
@@ -89,3 +150,53 @@ function castPokemon(pokemon) {
   // .then(pokemon => renderPokemon(pokemon)
   // ) pessimistic rendering
 }
+
+function createCommentForm() {
+  // select element to append form to
+  // const form = document.querySelector("#comments-form")
+  // create a form with a content field
+  const commentForm = document.createElement("form");
+  commentForm.id = "comment-form";
+  commentForm.addEventListener("submit", createComment);
+
+  const contentInput = document.createElement("input");
+  contentInput.type = "text";
+  contentInput.id = "content-input";
+
+  const submit = document.createElement("input");
+  submit.type = "submit";
+  submit.id = "submit";
+  submit.value = "Create Comment";
+
+  // formDiv.appendChild(contentInput)
+  commentForm.append(contentInput, submit);
+  formDiv.appendChild(commentForm);
+  // appendChild we can only pass in 1 node
+  // add an event listener to the form upon submit
+  // once the form is submitted, need to make a request
+}
+
+// event handler for comment form
+function createComment(e) {
+  e.preventDefault();
+
+  const comment = { content: e.target.querySelector("#content-input").value };
+
+  // make a post request
+  fetch("http://localhost:3000/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(comment)
+  })
+    .then(resp => resp.json())
+    .then(data => console.log(data));
+}
+
+
+// we want to limit how many likes a pokemon gets, to like 50 
+
+// which pokemon has 50 likes, return that pokemon
+
+// identify where this should happen
